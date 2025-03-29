fd05ee936d9e08c2072da53289039ac670be9afc
tencalink0 <bencax270@gmail.com>
2025-03-29 23:59:54 +0000

    first push

---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld 
+{
+    class Program 
+    {
+        static double ToFarenheit(double number) 
+        {
+            return (number * 9/5) + 32;
+        }
+
+        static double toCelsius(double number) 
+        {
+            return (number - 32) * 5/9;
+        }
+
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Enter a temperature in degrees");
+            double userInput = double.parse(Console.ReadLine());
+
+            Console.WriteLine("Celcius - Farenheit" + ToFarenheit(userInput));
+            Console.WriteLine("Farenheit - Celsius" + ToFarenheit(userInput));
+        }
+    }
+}
